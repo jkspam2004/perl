4dfc77775dba0403cfbe7be91c7aad18179bd695
jkspam2004 <jkspam2004@yahoo.com>
2015-01-25 22:40:08 -0800

    best approach for product of all ints except at index

--- bin/product_of_ints.pl
+++ bin/product_of_ints.pl
@@ -4,16 +4,45 @@ use strict;
 use Data::Dumper;
 $Data::Dumper::Sortkeys = 1;
 
-&get_products_of_all_ints_except_at_index;
+&get_products_of_all_ints_except_at_index_best;
+#&get_products_of_all_ints_except_at_index;
 #&get_products_of_all_ints_except_at_index_brute;
 
-# greedy approach 3*O(n) or just O(n)
+# only need one array to store the final product
+# O(n) for time and O(n) for space
+sub get_products_of_all_ints_except_at_index_best {
+    my @integers = (1, 2, 3, 4, 5);
+
+    my %final_product = ();
+    my $product = 1;
+    my $i = 0;
+    while ( $i <= $#integers ) {
+        $final_product{$i} = $product; 
+        $product *= $integers[$i];
+        $i++;
+    }
+
+    $product = 1;
+    $i = $#integers;
+    while ( $i >= 0 ) {
+        $final_product{$i} = $final_product{$i} * $product; 
+        $product *= $integers[$i];
+        $i--;
+    }
+
+    print "product_of_ints_except_at_index=".  Dumper(\%final_product);
+}
+
+# greedy approach just O(n) for time
+# but too many arrays - three O(n) memory cost. 1 array for product before index, 1 array for product after index, 1 for finall product
 sub get_products_of_all_ints_except_at_index {
     my @integers = (1, 2, 3, 4, 5);
     #            0   1   2   3   4        0        1        2        3        4
     # result = [120, 60, 40, 30, 24] : [2*3*4*5, 1*3*4*5, 1*2*4*5, 1*2*3*5, 1*2*3*4]
     #my @integers = (1, 2, 0, 4, 5);
     #[0, 0, 40, 0, 0]
+
+    print "integers= @integers\n";
 
     my $prod = 1;
     # get the product of all integers before each index
@@ -24,7 +53,7 @@ sub get_products_of_all_ints_except_at_index {
         $before{$i} = $prod;
         $prod *= $integers[$i];
     }
-    print Dumper(\%before);
+    print "product_of_ints_before=" . Dumper(\%before);
 
     # get the product of all integers after each index
     #             0       1     2   3  4
@@ -35,13 +64,13 @@ sub get_products_of_all_ints_except_at_index {
         $after{$i} = $prod;
         $prod *= $integers[$i];
     }
-    print Dumper(\%after);
+    print "product_of_ints_after=" . Dumper(\%after);
 
     my %product = ();
     for my $i ( 0 .. $#integers ) {
         $product{$i} = $before{$i} * $after{$i};
     }
-    print Dumper(\%product);
+    print "product_of_ints_except_at_index=".  Dumper(\%product);
 
     return;
 }
